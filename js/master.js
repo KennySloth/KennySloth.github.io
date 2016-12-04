c9ff5fe5036649f8560f53b11cef1f9d1fe11cef
KennySloth <skapsloth@gmail.com>
2016-12-04 00:21:44 -0800

    lots of changes to the script

--- js/master.js
+++ js/master.js
@@ -1,8 +1,8 @@
-function deport(){
-	alert("deported, cunt");
-	window.open('https://healingpenn.files.wordpress.com/2014/06/man-coming-out-of-the-wall2.jpg');
+function Meme(){
+	alert("Dank");
+	window.open('https://www.youtube.com/watch?v=dprHL6nm18o');
 }
-function SnowMen(){
+function Even More Meme(){
 	alert("im very sorry but we dont want you by our  snowmen or our milk because your going to take all of it");
 	window.open('http://www.flossiesgifts.com/v/vspfiles/photos/JS-4023464-2.jpg')
 }
