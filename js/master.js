86bf64d907015d1ed8c1884fece7101e367db43f
KennySloth <skapsloth@gmail.com>
2016-12-04 05:22:28 -0800

    alittle more fixes

--- js/master.js
+++ js/master.js
@@ -2,7 +2,7 @@ function Deported(){
 	alert("Raped");
 	window.open('https://www.migrant-rights.org/wp-content/themes/migrantrights/images/_campaigns/scene-01-stamp.png');
 }
-function Even More Meme(){
+function Canada(){
 	alert("im very sorry but we dont want you by our  snowmen or our milk because your going to take all of it");
 	window.open('http://www.flossiesgifts.com/v/vspfiles/photos/JS-4023464-2.jpg')
 }
